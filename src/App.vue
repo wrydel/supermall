6f761a4db91f889b07a08bf274d4ef23f8f757c9
wrydel <527573878@qq.com>
2021-06-06 20:16:11 +0800

    同步公司修改的

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-
-=======
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
->>>>>>> 507c556fc7b4557e476d08261cfad81928d2e71a
   </div>
 </template>
 
